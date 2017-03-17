30f29106a6fc761896ba63a349d8e0c2c408a26c
shwetha <shwethk4@in.ibm.com>
2017-03-17 15:37:28 +0530

    initial commit

---
+++ src/main/java/myProject/simple.java
@@ -0,0 +1,12 @@
+package myProject;
+
+public class simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hi this is the first project");
+
+	}
+
+}
